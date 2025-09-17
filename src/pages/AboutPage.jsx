af3fb79488832793c3ee9779a7eaa5be7b4cdd75
rgreyesph <roger@pisas.herkings.com>
2025-09-17 13:52:36 +0800

    Fixed the AboutPage syntax error

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -6,11 +6,10 @@ function AboutPage() {
     <div>
       <h1>Welcome to Herkings.com!</h1>
       <p>This is the about us page.</p>
-        <div>
-        <p>HERKINGS CORPORATION is a member of Powerhaus Industrial Sales And Services, Inc., established in 2003, with the aim of providing parts and equipment sales, maintenance services and latest technology solutions to the power, energy and heavy industries in the Philippines.
-  We are HERKINGS CORPORATION is a member of Powerhaus Industrial Sales And Services, Inc., established in 2003, with the aim of providing parts and equipment sales, maintenance services and latest technology solutions to the power, energy and heavy industries in the Philippines.
-  We are authorized or exclusive Philippine distributor of some of the world's leading engineering companies in the Power Industry.</p> or exclusive Philippine distributor of some of the world's leading engineering companies in the Power Industry.</p>
-      </div>
+      <p>HERKINGS CORPORATION is a member of Powerhaus Industrial Sales And Services, Inc., established in 2003, with the aim of providing parts and equipment sales, maintenance services and latest technology solutions to the power, energy and heavy industries in the Philippines.
+      We are HERKINGS CORPORATION is a member of Powerhaus Industrial Sales And Services, Inc., established in 2003, with the aim of providing parts and equipment sales, maintenance services and latest technology solutions to the power, energy and heavy industries in the Philippines.
+      We are authorized or exclusive Philippine distributor of some of the world's leading engineering companies in the Power Industry. or exclusive Philippine distributor of some of the world's leading engineering companies in the Power Industry.
+      </p>
     </div>
   );
 }
