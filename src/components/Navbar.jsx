1a10093e950340fee48d0afc6623ac1ad3c9395f
rgreyesph <roger@pisas.herkings.com>
2025-09-17 13:41:13 +0800

    Update navbar to adopt to mobile phone screen

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './Navbar.css'; // We will create this CSS file next
+import './Navbar.css'; // We will update this file next
 
 function Navbar() {
+  // 'useState' is a React Hook to manage the open/closed state of the menu
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  // This function will be called when a link is clicked on mobile to close the menu
+  const handleLinkClick = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
     <nav className="navbar">
-      <Link to="/" className="nav-logo">Herkings.com</Link>
-      <ul className="nav-menu">
-        <li className="nav-item"><Link to="/" className="nav-link">Home</Link></li>
-        <li className="nav-item"><Link to="/about" className="nav-link">About Us</Link></li>
+      <Link to="/" className="nav-logo" onClick={handleLinkClick}>
+        Herkings.com
+      </Link>
+      
+      {/* This is the Hamburger Icon */}
+      <div className="hamburger" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </div>
+
+      {/* We add the 'open' class conditionally based on the state */}
+      <ul className={isMenuOpen ? "nav-menu open" : "nav-menu"}>
         <li className="nav-item">
-          <div className="dropdown">
-            <button className="dropbtn">Offerings</button>
-            <div className="dropdown-content">
-              <Link to="/products">Products</Link>
-              <Link to="/services">Services</Link>
-            </div>
+          <Link to="/" className="nav-link" onClick={handleLinkClick}>Home</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/about" className="nav-link" onClick={handleLinkClick}>About Us</Link>
+        </li>
+        <li className="nav-item dropdown">
+          <div className="dropbtn">Offerings</div>
+          <div className="dropdown-content">
+            <Link to="/products" onClick={handleLinkClick}>Products</Link>
+            <Link to="/services" onClick={handleLinkClick}>Services</Link>
           </div>
         </li>
-        <li className="nav-item"><Link to="/gallery" className="nav-link">Gallery</Link></li>
-        <li className="nav-item"><Link to="/blog" className="nav-link">Blog</Link></li>
-        <li className="nav-item"><Link to="/contact" className="nav-link">Contact Us</Link></li>
+        <li className="nav-item">
+          <Link to="/gallery" className="nav-link" onClick={handleLinkClick}>Gallery</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/blog" className="nav-link" onClick={handleLinkClick}>Blog</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/contact" className="nav-link" onClick={handleLinkClick}>Contact Us</Link>
+        </li>
       </ul>
     </nav>
   );
