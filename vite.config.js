7fe0f7c9f13f73275a5534995a0625ea7fbaf8f3
rgreyesph <roger@pisas.herkings.com>
2025-09-22 19:01:49 +0800

    fix: Add optimizeDeps to vite config for Amplify

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Explicitly set the base path for the project
+  base: '/',
+  optimizeDeps: {
+    include: [
+      '@aws-amplify/api/rest',
+      '@aws-amplify/auth',
+      '@aws-amplify/ui-react'
+    ],
+  },
 })
